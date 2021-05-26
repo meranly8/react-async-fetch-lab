2a25352e3cf3979b8e01a151a9ca1093292f32bc
Michelle Ranly <meranly8@gmail.com>
2021-05-26 01:59:20 -0500

    commit tree 75ad7feb779ae8dc084f6f420f0e2eb413a17cb7

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import React, {Component} from 'react'
+
+export default class App extends Component {
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(resp => resp.json())
+        .then(json => {
+            debugger
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <p>Fetch Lab</p>
+            </div>
+        )
+    }
+}
